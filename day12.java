fc104b9a1e37449e9aa7c5b8d79db2548530e699
Gargi Rajadhyaksha <grajadhyaksha@atlmbp195.local>
2016-12-21 12:52:06 -0500

    Code cleanup

--- day12.java
+++ day12.java
@@ -14,7 +14,6 @@ public class day12 {
         while (line != null)
         {
           sb.append(line);
-        //  System.out.println(line);
           sb.append(System.lineSeparator());
           line = br.readLine();
         }
@@ -24,14 +23,14 @@ public class day12 {
         registers1.put("b", 0);
         registers1.put("c", 0);
         registers1.put("d", 0);
-      //  System.out.println("Result = " + solve(firstPart, registers));
+        System.out.println("Result of second part = " + solve(firstPart, registers));
         String secondPart = sb.toString();
         HashMap<String, Integer> registers2 = new HashMap<String, Integer>();
         registers2.put("a", 0);
         registers2.put("b", 0);
         registers2.put("c", 1);
         registers2.put("d", 0);
-        System.out.println("Result = " + solve(secondPart, registers2));
+        System.out.println("Result of first part = " + solve(secondPart, registers2));
 
 }
     }
@@ -43,14 +42,11 @@ public class day12 {
 
       while(i >= 0 && i < cmds.length)
       {
-        System.out.println(cmds[i]);
-
         if(cmds[i].startsWith("cpy"))
         {
            String[] inputCpy = cmds[i].split("\\s+");
            if (inputCpy[1].matches("\\d+"))
            {
-             //System.out.println("here");
              registers.put(inputCpy[2], Integer.parseInt(inputCpy[1]));
            }
            else
@@ -62,7 +58,6 @@ public class day12 {
 
         else if(cmds[i].startsWith("inc"))
         {
-          System.out.println("there");
             String[] inputInc = cmds[i].split("\\s+");
             registers.put(inputInc[1], registers.get(inputInc[1]) + 1);
             i++;
@@ -89,12 +84,7 @@ public class day12 {
                 i++;
             else
             {
-              System.out.println("Here is the value of b "+registers.get("b"));
-              System.out.println("Here is the value of a "+registers.get("a"));
-              System.out.println("Here is the value of c "+registers.get("c"));
-              System.out.println("Here is the value of d "+registers.get("d"));
               i = i + Integer.parseInt(inputJnz[2]);
-              System.out.println(i);
             }
         }
 
